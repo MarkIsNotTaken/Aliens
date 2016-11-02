30637bbf2687db4b245c1644cc075f52953681a2
MarkIsNotTaken <mdemesillo@gmail.com>
2016-11-02 08:48:45 -1000

    Aliens.js

---
+++ Aliens.js
@@ -0,0 +1,39 @@
+var Blue = 3000;
+
+var Green = 5001;
+
+var Red = 1999;
+
+function alienArmy (){
+	if(Blue + Green + Red == 10000);
+}
+
+function theGreens () {
+	if(Green > 5000){
+		console.log("MOTHER OF GOD, not the green ones");
+	}
+}
+
+function theRedandGreens () {
+	if(Green + Red = 8000){
+		console.log("DOOOOOM!");
+	}
+	else{
+		console.log("That's 1000 away from 8000, but we still dead.");
+	}
+}
+
+function theRedandBlues() {
+	if(Red + Blue != 3000){
+		console.log("We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!");
+	}
+}
+
+
+
+
+
+
+
+
+
